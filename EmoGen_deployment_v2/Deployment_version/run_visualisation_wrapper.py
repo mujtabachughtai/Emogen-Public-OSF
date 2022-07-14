fccdf924687a07cfd9aa8218325a2f8c00677d54
alex <alexandrosrotsidis@icloud.com>
2022-07-14 20:03:28 +0300

    minor file renames for renderer

--- EmoGen_deployment_v2/Deployment_version/run_visualisation_wrapper.py
+++ EmoGen_deployment_v2/Deployment_version/run_visualisation_wrapper.py
@@ -4,10 +4,9 @@ import subprocess
 import sys
 
 csv_file = sys.argv[1] # /home/emogen/emogenWeb/shared/public/111/1655833333538/HAPPY/MALE/initialisation.csv
-save_dir = sys.argv[2] # /home/emogen/emoGen_sandbox/EmoGen_deployment_v2/Deployment_version/user_output/
+save_dir = sys.argv[2] # /home/emogen/emoGen_sandbox/EmoGen_deployment_v2/Deployment_version/user_output/initialisation_transposed.csv
 csv_data = genfromtxt(csv_file, delimiter=',')
 
-import pdb
-pdb.set_trace()
+np.savetxt(save_dir, np.transpose(csv_data), delimiter=",")
 
 subprocess.run(["run_visualisation.sh"])
